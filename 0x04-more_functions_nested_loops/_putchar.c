92c4113d0d767ddd5fb30342e4db4974ece45645
martinondiwa <surekondiwa@gmail.com>
2023-10-12 13:18:07 -0700

    print numbers out

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+int _putchar(char c)
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+{
+    return write(1, &c, 1);
+}
