9d24825af3d49b217fbaa99ab98f349e35c2d231
martinondiwa <surekondiwa@gmail.com>
2023-11-16 15:30:02 -0800

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,19 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+char *my_strdup(const char *s)
+{
+	size_t len = strlen(s) + 1;
+	char *dup = malloc(len);
+
+	if (dup != NULL)
+	{
+		strcpy(dup, s);
+	}
+
+	return (dup);
+}
 
 /**
  * add_node - Adds a new node at the beginning.
@@ -15,7 +30,8 @@ list_t *add_node(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	new->str = strdup(str);
+
+	new->str = my_strdup(str);
 
 	for (nchar = 0; str[nchar]; nchar++)
 		;
