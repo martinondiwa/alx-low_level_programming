e6d09009c15229ab10b45400eccbafe29e0ad47b
martinondiwa <surekondiwa@gmail.com>
2023-11-16 14:32:07 -0800

    lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list
- * @h: singly linked list.
- * Return: number of elements in the list
+ * print_list - Prints the elements of a linked list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	int count = 0;
 
-	nelem = 0;
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+		{
+			printf("[0] (nil)\n");
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
 		h = h->next;
-		nelem++;
 	}
-	return (nelem);
+
+	return (count);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,28 +1,21 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef LISTS_H
+#define LISTS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stddef.h>
 
-/**
- * struct list_s - Definition of a singly linked list node.
- * @str: String stored in the node (allocated using malloc).
- * @len: Length of the string.
- * @next: Pointer to the next node in the list.
- *
- * Description: Represents a node in a singly linked list.
- */
-typedef struct listint_s
+/* Structure for a singly linked list */
+typedef struct list_s
 {
-	int n;
-	struct listint_s *next;
-} listint_t;
+	char *str;
+	size_t len;
+	struct list_s *next;
+} list_t;
 
+/* Function prototypes */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* _LISTS_H_ */
+#endif /* LISTS_H */
