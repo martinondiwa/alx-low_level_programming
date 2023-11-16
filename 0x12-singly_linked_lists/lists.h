46621d119cb10b4ac3f34b827cd4d7c1511c3ae4
martinondiwa <surekondiwa@gmail.com>
2023-11-16 12:09:14 -0800

    lists5

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,24 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
-int _putchar(char c);
+/**
+ * struct list_s - Definition of a singly linked list node.
+ * @str: String stored in the node (allocated using malloc).
+ * @len: Length of the string.
+ * @next: Pointer to the next node in the list.
+ *
+ * Description: Represents a node in a singly linked list.
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
@@ -12,4 +26,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* MAIN_H */
+#endif /* _LISTS_H_ */
