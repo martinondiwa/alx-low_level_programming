22b2e36b9ff7f5e464290bf6b0bad15d606d3e58
martinondiwa <surekondiwa@gmail.com>
2024-08-12 18:39:43 -0700

    copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,105 +1,76 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <buffer.c>
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
 
-
-char *create_buffer(char *file);
-void close_file(int fd);
+#define BUF_SIZE 1024
 
 /**
- * creates_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
- *
- * Return: A pointer to the newly-allocated buffer.
+ * print_error - prints error messages to stderr and exits
+ * @message: error message to print
+ * @exit_code: exit code to use
  */
-char *creates_buffer(char *file)
+void print_error(const char *message, int exit_code)
 {
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", file);
-		exit(99);
-	}
-
-	return (buffer);
+	dprintf(STDERR_FILENO, "%s\n", message);
+	exit(exit_code);
 }
 
 /**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
+ * copy_file - copies content from one file to another
+ * @file_from: file descriptor of the source file
+ * @file_to: file descriptor of the destination file
  */
-void close_file(int fd)
+void copy_file(int file_from, int file_to)
 {
-	int c;
+	char buf[BUF_SIZE];
+	int num_read, num_written;
 
-	c = close(fd);
-
-	if (c == -1)
+	while ((num_read = read(file_from, buf, sizeof(buf))) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
+		num_written = write(file_to, buf, num_read);
+		if (num_written < num_read)
+			print_error("Error: Can't write to file", 99);
 	}
+
+	if (num_read == -1)
+		print_error("Error: Can't read from file", 98);
 }
 
 /**
- * main - Copies the contents of a file to another file.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * main - program that copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: argument strings
  *
- * Return: 0 on success.
- *
- * Description: If the argument count is incorrect - exit code 97.
- * If file_from does not exist or cannot be read - exit code 98.
- * If file_to cannot be created or written to - exit code 99.
- * If file_to or file_from cannot be closed - exit code 100.
+ * Return: 0 on success, or a different exit code on error
  */
 int main(int argc, char *argv[])
 {
-	int from, to, r, w;
-	char *buffer;
+	int file_from, file_to;
 
 	if (argc != 3)
+		print_error("Usage: cp file_from file_to", 97);
+
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+		print_error("Error: Can't read from file", 98);
+
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR |
+			S_IRGRP | S_IWGRP | S_IROTH);
+	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		close(file_from);
+		print_error("Error: Can't write to file", 99);
 	}
 
-	buffer = create_buffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	copy_file(file_from, file_to);
 
-	do {
-		if (from == -1 || r == -1)
-		{
-			dprintf(STDERR_FILENO,
-					"Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			exit(98);
-		}
+	if (close(file_from) == -1)
+		print_error("Error: Can't close fd", 100);
 
-		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
-		{
-			dprintf(STDERR_FILENO,
-					"Error: Can't write to %s\n", argv[2]);
-			free(buffer);
-			exit(99);
-		}
-
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
-
-	} while (r > 0);
-
-	free(buffer);
-	close_file(from);
-	close_file(to);
+	if (close(file_to) == -1)
+		print_error("Error: Can't close fd", 100);
 
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#ifndef FILE_IO
+#define FILE_IO
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -10,6 +16,10 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-char *create_buffer(char *file)
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
+char *create_buffer(char *file);
 #endif
