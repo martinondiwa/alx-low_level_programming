9f3350abd1a3268cef680e5ce3fd5b500afe5183
martinondiwa <surekondiwa@gmail.com>
2024-08-12 17:50:32 -0700

    Create buffer file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <buffer.h>
+
 
 char *create_buffer(char *file);
 void close_file(int fd);
---
+++ 0x15-file_io/buffer.h
@@ -0,0 +1,9 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+
+#include <stdlib.h>  // Include standard library for malloc
+
+// Function declaration for create_buffer
+char *create_buffer(char *file);
+
+#endif /* BUFFER_H */
